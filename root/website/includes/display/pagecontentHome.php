d8a627854cab4d7e0311166f711432d0cf89a092
tom-1 <tom-1@22cb28ec-0345-0410-b6dc-f8d001edd02c>
2008-03-28 20:37:01 +0000

    Tracker bug Request ID 1928000 Typo on OQ website home page
    git-svn-id: svn://svn.code.sf.net/p/oquote/code@40 22cb28ec-0345-0410-b6dc-f8d001edd02c

--- root/website/includes/display/pagecontentHome.php
+++ root/website/includes/display/pagecontentHome.php
@@ -7,7 +7,7 @@
 			
 				<h3>OpenQuote</h3>
 				<p><a href="http://sourceforge.net/projects/oquote/"><img src="images/openquoteicon.png" width="100" height="105" alt="OpenQuote icon"  class="float-left" /></a></p>
-				<p>OpenQuote is an <a href="opensource.php">open source</a>, web based, insurance quotation solution which allows quotes to be produced, and products maintained, over the web. Any number of products can be configured to support any line of business via externalised product definitions and rules. For more information please feel free to browse this site or ask questions in out project forums.</p>
+				<p>OpenQuote is an <a href="opensource.php">open source</a>, web based, insurance quotation solution which allows quotes to be produced, and products maintained, over the web. Any number of products can be configured to support any line of business via externalised product definitions and rules. For more information please feel free to browse this site or ask questions in our project forums.</p>
 				<br/><br/><br/>
 				
 			</div>
