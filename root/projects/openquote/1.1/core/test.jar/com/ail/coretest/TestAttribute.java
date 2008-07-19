cd253f7f3fbd7773da0748cb2fbc0ba683ed8e93
dickanderson <dickanderson@22cb28ec-0345-0410-b6dc-f8d001edd02c>
2008-07-19 11:14:32 +0000

    Attribute class's value property moved from an XML attribute to text
    git-svn-id: svn://svn.code.sf.net/p/oquote/code@125 22cb28ec-0345-0410-b6dc-f8d001edd02c

--- root/projects/openquote/1.1/core/test.jar/com/ail/coretest/TestAttribute.java
+++ root/projects/openquote/1.1/core/test.jar/com/ail/coretest/TestAttribute.java
@@ -23,14 +23,15 @@ import junit.framework.TestSuite;
 import junit.textui.TestRunner;
 
 import com.ail.core.Attribute;
+import com.ail.core.CoreProxy;
+import com.ail.core.XMLString;
 
 /**
  * @version $Revision$
  * @author $Author$
  * @state $State$
  * @date $Date$
- * @source $Source:
- *         /home/bob/CVSRepository/projects/core/test/com/ail/coretest/TestTypeXpath.java,v $
+ * @source $Source: /home/bob/CVSRepository/projects/core/test/com/ail/coretest/TestTypeXpath.java,v $
  */
 public class TestAttribute extends TestCase {
 
@@ -50,7 +51,7 @@ public class TestAttribute extends TestCase {
     public void testAttributeValidation() {
         Attribute attr;
 
-        attr=new Attribute("q1","1","number;min=0;max=21");
+        attr = new Attribute("q1", "1", "number;min=0;max=21");
         attr.setValue("-1");
         assertTrue(attr.isInvalid());
         attr.setValue("22");
@@ -58,23 +59,23 @@ public class TestAttribute extends TestCase {
         attr.setValue("10");
         assertTrue(!attr.isInvalid());
 
-        attr=new Attribute("q1","1","currency;min=0.20;max=21", "GBP");
+        attr = new Attribute("q1", "1", "currency;min=0.20;max=21", "GBP");
         attr.setValue("0.12");
         assertTrue(attr.isInvalid());
         attr.setValue("22.54");
         assertTrue(attr.isInvalid());
         attr.setValue("10");
         assertTrue(!attr.isInvalid());
-        
-        attr=new Attribute("q1", "hello", "string;min=4;max=20");
+
+        attr = new Attribute("q1", "hello", "string;min=4;max=20");
         attr.setValue("ooo");
         assertTrue(attr.isInvalid());
         attr.setValue("ooooooooooooooooooooo");
         assertTrue(attr.isInvalid());
         attr.setValue("ooooooooooooo");
         assertTrue(!attr.isInvalid());
-        
-        attr=new Attribute("q1", "hello", "string;min=0;max=20");
+
+        attr = new Attribute("q1", "hello", "string;min=0;max=20");
         attr.setValue("");
         assertTrue(!attr.isInvalid());
         attr.setValue("oooooooooo");
@@ -82,44 +83,92 @@ public class TestAttribute extends TestCase {
         attr.setValue("ooooooooooooooooooooo");
         assertTrue(attr.isInvalid());
 
-        attr=new Attribute("q1", "hello", "string;pattern=[0-9a-z]*");
+        attr = new Attribute("q1", "hello", "string;pattern=[0-9a-z]*");
         attr.setValue("abc123");
         assertTrue(!attr.isInvalid());
         attr.setValue("Aabc123");
         assertTrue(attr.isInvalid());
 
-        attr=new Attribute("q1", "30/11/1978", "date;pattern=dd/MM/yyyy");
+        attr = new Attribute("q1", "30/11/1978", "date;pattern=dd/MM/yyyy");
         attr.getObject();
     }
 
     public void testAttributeFormatting() {
         Attribute attr;
 
-       attr=new Attribute("amount5", "£12.234", "number;pattern=£#.##");
-       assertEquals("£12.23", attr.getFormattedValue());
-       
+        attr = new Attribute("amount5", "£12.234", "number;pattern=£#.##");
+        assertEquals("£12.23", attr.getFormattedValue());
     }
-    
+
     public void testReference() {
-       Attribute attr;
+        Attribute attr;
 
-       Attribute.setReferenceContext(new Attribute("refdata", "?", "choice,options=-1#?|1#Salon|2#Coupe|3#Convertible"));
-       
-       attr=new Attribute("id123", "?", "ref=/format");
-       assertFalse(attr.isInvalid());
-       
-       attr.setValue("Salon");
-       assertFalse(attr.isInvalid());
+        Attribute.setReferenceContext(new Attribute("refdata", "?", "choice,options=-1#?|1#Salon|2#Coupe|3#Convertible"));
 
-       attr.setValue("Hello");
-       assertTrue(attr.isInvalid());
+        attr = new Attribute("id123", "?", "ref=/format");
+        assertFalse(attr.isInvalid());
+
+        attr.setValue("Salon");
+        assertFalse(attr.isInvalid());
+
+        attr.setValue("Hello");
+        assertTrue(attr.isInvalid());
     }
 
     /**
      * Test that commas in a format option list are seen as part of the option text itself, and not a format separator.
      */
     public void testChoiceOptionWithCommas() {
-        Attribute a=new Attribute("refdata", "?", "choice,options=-1#?|1#Salon|2#House, Boat|3#Convertible, ship");
+        Attribute a = new Attribute("refdata", "?", "choice,options=-1#?|1#Salon|2#House, Boat|3#Convertible, ship");
         assertEquals("-1#?|1#Salon|2#House, Boat|3#Convertible, ship", a.getFormatOption("options"));
+    }
+
+    /**
+     * Test choice attribute formatting
+     */
+    public void testChoiceFormatting() {
+        Attribute a = new Attribute("refdata", "Convertible, ship", "choice,options=-1#?|1#Salon|2#House, Boat|3#Convertible, ship");
+        assertEquals("Convertible, ship", a.getFormattedValue());
+        assertEquals("Convertible, ship", a.getValue());
+        assertEquals(3.0, a.getObject());
+
+    }
+
+    public void testCurrencyAttribute() {
+        Attribute attr = new Attribute("q1", "1002.23", "currency", "GBP");
+
+        assertEquals(java.lang.String.class, attr.getValue().getClass());
+        assertEquals(java.lang.Double.class, attr.getObject().getClass());
+
+        assertEquals("1,002.23 GBP", attr.getFormattedValue());
+        assertEquals("1002.23", attr.getValue());
+        assertEquals(1002.23, attr.getObject());
+        System.out.print(attr.getObject().getClass().getName());
+    }
+    
+    /**
+     * In versions of the core up to and including 2.2 the attribute value property was always mapped to an xml attribute. For 2.3
+     * the value was moved to the text of the element to allow for markup within the value - particularly for attributes of type "note".
+     * This test checks that the new approach works, and that backward compatibility has been maintained - i.e. that 2.3 will 
+     * correctly read Xml generated from pre-2.3 systems.
+     */
+    public void testXmlTextMapping() throws Exception{
+    	Attribute attr;
+    	String pre23Sample="<attribute xsi:type='com.ail.core.Attribute'  xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' id='name' format='note' value='hello'/>";
+    	String post23Sample="<attribute xsi:type='com.ail.core.Attribute'  xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' id='name' format='note'>My Hat Is Off</attribute>";
+
+    	attr=new CoreProxy().fromXML(Attribute.class, new XMLString(pre23Sample));
+    	assertEquals("name", attr.getId());
+    	assertEquals("note", attr.getFormat());
+    	assertEquals("hello", attr.getValue());
+
+    	attr=new CoreProxy().fromXML(Attribute.class, new XMLString(post23Sample));
+    	assertEquals("name", attr.getId());
+    	assertEquals("note", attr.getFormat());
+    	assertEquals("My Hat Is Off", attr.getValue());
+    	
+    	String xml=new CoreProxy().toXML(attr).toString();
+    	assertTrue(xml.contains(">My Hat Is Off<"));
+    	assertFalse(xml.contains("\"My Hat Is Off\""));
     }
 }
