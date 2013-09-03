56723f549ba0b9d8e7c8f4e477c287c8e343083f
dickanderson <dickanderson@22cb28ec-0345-0410-b6dc-f8d001edd02c>
2013-09-03 07:16:32 +0000

    OQ-1: Rebranding to OpenUnderwriter - stage 3 - Fixes up users and added new portal instances.
    git-svn-id: svn://svn.code.sf.net/p/oquote/code@1059 22cb28ec-0345-0410-b6dc-f8d001edd02c

--- root/projects/openunderwriter/2.0/package/bin/run.bat
+++ root/projects/openunderwriter/2.0/package/bin/run.bat
@@ -1,9 +1,9 @@
 @echo off
 
-set OPENQUOTE_HOME=%CD%\..
-set TMP=%OPENQUOTE_HOME%\tmp
-set LIB=%OPENQUOTE_HOME%\lib
-set JBOSS_HOME=%OPENQUOTE_HOME%\liferay-portal-@liferay.version@/jboss-@jboss.version@
+set OPENUNDERWRITER_HOME=%CD%\..
+set TMP=%OPENUNDERWRITER_HOME%\tmp
+set LIB=%OPENUNDERWRITER_HOME%\lib
+set JBOSS_HOME=%OPENUNDERWRITER_HOME%\liferay-portal-@liferay.version@/jboss-@jboss.version@
 set PROGNAME=run.bat
 set DB_USERNAME=
 set DB_PASSWORD=
@@ -11,13 +11,13 @@ set DB_PASSWORD=
 if exist "%TMP%\setup" goto setupdone
 
 echo.
-echo OpenQuote Database Setup
-echo ========================
-echo The first time that you run OpenQuote, this script will create a database in 
+echo OpenUnderwriter Database Setup
+echo ==============================
+echo The first time that you run OpenUnderwriter, this script will create a database in 
 echo MySQL and populate it with content, it will also create an openquote database
 echo user. To do this successfully the script will need the username and password 
 echo of a user who has the necessary permissions to create database and users.
-echo This user will only be used to run the scripts. The OpenQuote server itself
+echo This user will only be used to run the scripts. The OpenUnderwriter server itself
 echo will use the less privileged user created by the scripts.
 echo. 
 echo You will not be prompted for these user details again.
@@ -35,12 +35,12 @@ echo Running database scripts...
 mysql -u %DB_USERNAME% %PW_OPTION% < "%LIB%\OpenQuote-MySql-Setup.sql"
 if "%ERRORLEVEL%" == "1" (echo "Failed to execute the MySQL database setup script." & goto:eof)
 mysql -u %DB_USERNAME% %PW_OPTION% < "%LIB%\OpenQuote-Table-Setup.sql"
-if "%ERRORLEVEL%" == "1" (echo "Failed to execute the MySQL OpenQuote table setup script." & goto:eof)
+if "%ERRORLEVEL%" == "1" (echo "Failed to execute the MySQL OpenUnderwriter table setup script." & goto:eof)
     
 mkdir %TMP%
 echo > %TMP%\setup
     
-echo OpenQuote setup complete. Starting JBoss...
+echo OpenUnderwriter setup complete. Starting JBoss...
 
 :setupdone
 
