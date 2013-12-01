1b12030b40d6f15ea33350bbf9fd2d6dd1e2994b
dickanderson <dickanderson@22cb28ec-0345-0410-b6dc-f8d001edd02c>
2013-12-01 23:20:22 +0000

    OU-1: OpenActuary DBs versioned
    git-svn-id: svn://svn.code.sf.net/p/oquote/code@1170 22cb28ec-0345-0410-b6dc-f8d001edd02c

--- root/projects/openunderwriter/2.0/database/content/OpenActuary-Database-Setup.sql
+++ root/projects/openunderwriter/2.0/database/content/OpenActuary-Database-Setup.sql
@@ -1,45 +1,45 @@
 -- Create example actuarial databases for use by OpenActuary users
-create database if not exists AIL_Base_DataSource_Master_Motor character set utf8;
-grant insert,select,update,delete on AIL_Base_DataSource_Master_Motor.* to 'adam'@'localhost' identified by 'Dhj0TB/sA+xAg0IstYumoJrE+yo=';
-grant select on AIL_Base_DataSource_Master_Motor.* to 'charlie'@'localhost' identified by '2M0QuSDcvbUWPKAYXkAjV7wnwmU=';
-grant select on AIL_Base_DataSource_Master_Motor.* to 'lily'@'localhost' identified by '/dvgGFfi06tQW/wdrDs4osi4zWU=';
-grant select on AIL_Base_DataSource_Master_Motor.* to 'oliver'@'localhost' identified by 'xTkVO6H5R71Lb5ECY7lnxKCmI1c=';
-grant select on AIL_Base_DataSource_Master_Motor.* to 'ethan'@'localhost' identified by 'CifhLQYq1xZz1X+cJ5myB68xaIU=';
-grant select on AIL_Base_DataSource_Master_Motor.* to 'sophie'@'localhost' identified by 'X1BEO/529yeajg8vCpiXXNv/OOk=';
-grant select on AIL_Base_DataSource_Master_Motor.* to 'thomas'@'localhost' identified by 'X1CoTB+jvP8UZAUBfzauwaEKnjg=';
-grant select on AIL_Base_DataSource_Master_Motor.* to 'report'@'localhost' identified by 'e98d2f001da5678b39482efbdf5770dc';
-use AIL_Base_DataSource_Master_Motor;
+create database if not exists ##dbname##_AIL_Base_DataSource_Master_Motor character set utf8;
+grant insert,select,update,delete on ##dbname##_AIL_Base_DataSource_Master_Motor.* to 'adam'@'localhost' identified by 'Dhj0TB/sA+xAg0IstYumoJrE+yo=';
+grant select on ##dbname##_AIL_Base_DataSource_Master_Motor.* to 'charlie'@'localhost' identified by '2M0QuSDcvbUWPKAYXkAjV7wnwmU=';
+grant select on ##dbname##_AIL_Base_DataSource_Master_Motor.* to 'lily'@'localhost' identified by '/dvgGFfi06tQW/wdrDs4osi4zWU=';
+grant select on ##dbname##_AIL_Base_DataSource_Master_Motor.* to 'oliver'@'localhost' identified by 'xTkVO6H5R71Lb5ECY7lnxKCmI1c=';
+grant select on ##dbname##_AIL_Base_DataSource_Master_Motor.* to 'ethan'@'localhost' identified by 'CifhLQYq1xZz1X+cJ5myB68xaIU=';
+grant select on ##dbname##_AIL_Base_DataSource_Master_Motor.* to 'sophie'@'localhost' identified by 'X1BEO/529yeajg8vCpiXXNv/OOk=';
+grant select on ##dbname##_AIL_Base_DataSource_Master_Motor.* to 'thomas'@'localhost' identified by 'X1CoTB+jvP8UZAUBfzauwaEKnjg=';
+grant select on ##dbname##_AIL_Base_DataSource_Master_Motor.* to 'report'@'localhost' identified by 'e98d2f001da5678b39482efbdf5770dc';
+use ##dbname##_AIL_Base_DataSource_Master_Motor;
 source ./content/OpenActuary-Table-Setup.sql
 source ./content/OpenActuary-Standing-Data-Setup.sql
 source ./content/OpenActuary-Trigger-Setup.sql
 
-create database if not exists MetropolitanInsurance_Motor_Staging character set utf8;
-grant insert,select,update,delete on MetropolitanInsurance_Motor_Staging.* to 'charlie'@'localhost' identified by '2M0QuSDcvbUWPKAYXkAjV7wnwmU=';
-grant select on MetropolitanInsurance_Motor_Staging.* to 'lily'@'localhost' identified by '/dvgGFfi06tQW/wdrDs4osi4zWU=';
-grant select on MetropolitanInsurance_Motor_Staging.* to 'oliver'@'localhost' identified by 'xTkVO6H5R71Lb5ECY7lnxKCmI1c=';
-grant select on MetropolitanInsurance_Motor_Staging.* to 'report'@'localhost' identified by 'e98d2f001da5678b39482efbdf5770dc';
-use MetropolitanInsurance_Motor_Staging;
+create database if not exists ##dbname##_MetropolitanInsurance_Motor_Staging character set utf8;
+grant insert,select,update,delete on ##dbname##_MetropolitanInsurance_Motor_Staging.* to 'charlie'@'localhost' identified by '2M0QuSDcvbUWPKAYXkAjV7wnwmU=';
+grant select on ##dbname##_MetropolitanInsurance_Motor_Staging.* to 'lily'@'localhost' identified by '/dvgGFfi06tQW/wdrDs4osi4zWU=';
+grant select on ##dbname##_MetropolitanInsurance_Motor_Staging.* to 'oliver'@'localhost' identified by 'xTkVO6H5R71Lb5ECY7lnxKCmI1c=';
+grant select on ##dbname##_MetropolitanInsurance_Motor_Staging.* to 'report'@'localhost' identified by 'e98d2f001da5678b39482efbdf5770dc';
+use ##dbname##_MetropolitanInsurance_Motor_Staging;
 source ./content/OpenActuary-Table-Setup.sql
 source ./content/OpenActuary-Standing-Data-Setup.sql
 source ./content/OpenActuary-Trigger-Setup.sql
 
-create database if not exists StarInsurance_Motor_Staging  character set utf8;
-grant insert,select,update,delete on StarInsurance_Motor_Staging.* to 'ethan'@'localhost' identified by 'CifhLQYq1xZz1X+cJ5myB68xaIU=';
-grant select on StarInsurance_Motor_Staging.* to 'sophie'@'localhost' identified by 'X1BEO/529yeajg8vCpiXXNv/OOk=';
-grant select on StarInsurance_Motor_Staging.* to 'thomas'@'localhost' identified by 'X1CoTB+jvP8UZAUBfzauwaEKnjg=';
-grant select on StarInsurance_Motor_Staging.* to 'report'@'localhost' identified by 'e98d2f001da5678b39482efbdf5770dc';
-use StarInsurance_Motor_Staging;
+create database if not exists ##dbname##_StarInsurance_Motor_Staging  character set utf8;
+grant insert,select,update,delete on ##dbname##_StarInsurance_Motor_Staging.* to 'ethan'@'localhost' identified by 'CifhLQYq1xZz1X+cJ5myB68xaIU=';
+grant select on ##dbname##_StarInsurance_Motor_Staging.* to 'sophie'@'localhost' identified by 'X1BEO/529yeajg8vCpiXXNv/OOk=';
+grant select on ##dbname##_StarInsurance_Motor_Staging.* to 'thomas'@'localhost' identified by 'X1CoTB+jvP8UZAUBfzauwaEKnjg=';
+grant select on ##dbname##_StarInsurance_Motor_Staging.* to 'report'@'localhost' identified by 'e98d2f001da5678b39482efbdf5770dc';
+use ##dbname##_StarInsurance_Motor_Staging;
 source ./content/OpenActuary-Table-Setup.sql
 source ./content/OpenActuary-Standing-Data-Setup.sql
 source ./content/OpenActuary-Trigger-Setup.sql
 
-create database if not exists AIL_Base_DataSource_Test character set utf8;
-grant insert,select,update,delete on AIL_Base_DataSource_Test.* to 'adam'@'localhost' identified by 'Dhj0TB/sA+xAg0IstYumoJrE+yo=';
-grant select on AIL_Base_DataSource_Test.* to 'charlie'@'localhost' identified by '2M0QuSDcvbUWPKAYXkAjV7wnwmU=';
-grant select on AIL_Base_DataSource_Test.* to 'lily'@'localhost' identified by '/dvgGFfi06tQW/wdrDs4osi4zWU=';
-grant select on AIL_Base_DataSource_Test.* to 'oliver'@'localhost' identified by 'xTkVO6H5R71Lb5ECY7lnxKCmI1c=';
-grant select on AIL_Base_DataSource_Test.* to 'ethan'@'localhost' identified by 'CifhLQYq1xZz1X+cJ5myB68xaIU=';
-grant select on AIL_Base_DataSource_Test.* to 'sophie'@'localhost' identified by 'X1BEO/529yeajg8vCpiXXNv/OOk=';
-grant select on AIL_Base_DataSource_Test.* to 'thomas'@'localhost' identified by 'X1CoTB+jvP8UZAUBfzauwaEKnjg=';
-grant select on AIL_Base_DataSource_Test.* to 'report'@'localhost' identified by 'e98d2f001da5678b39482efbdf5770dc';
+create database if not exists ##dbname##_AIL_Base_DataSource_Test character set utf8;
+grant insert,select,update,delete on ##dbname##_AIL_Base_DataSource_Test.* to 'adam'@'localhost' identified by 'Dhj0TB/sA+xAg0IstYumoJrE+yo=';
+grant select on ##dbname##_AIL_Base_DataSource_Test.* to 'charlie'@'localhost' identified by '2M0QuSDcvbUWPKAYXkAjV7wnwmU=';
+grant select on ##dbname##_AIL_Base_DataSource_Test.* to 'lily'@'localhost' identified by '/dvgGFfi06tQW/wdrDs4osi4zWU=';
+grant select on ##dbname##_AIL_Base_DataSource_Test.* to 'oliver'@'localhost' identified by 'xTkVO6H5R71Lb5ECY7lnxKCmI1c=';
+grant select on ##dbname##_AIL_Base_DataSource_Test.* to 'ethan'@'localhost' identified by 'CifhLQYq1xZz1X+cJ5myB68xaIU=';
+grant select on ##dbname##_AIL_Base_DataSource_Test.* to 'sophie'@'localhost' identified by 'X1BEO/529yeajg8vCpiXXNv/OOk=';
+grant select on ##dbname##_AIL_Base_DataSource_Test.* to 'thomas'@'localhost' identified by 'X1CoTB+jvP8UZAUBfzauwaEKnjg=';
+grant select on ##dbname##_AIL_Base_DataSource_Test.* to 'report'@'localhost' identified by 'e98d2f001da5678b39482efbdf5770dc';
 
--- root/projects/openunderwriter/2.0/database/content/OpenActuary-Database-Teardown.sql
+++ root/projects/openunderwriter/2.0/database/content/OpenActuary-Database-Teardown.sql
@@ -1,8 +1,8 @@
 -- Drop the example actuarial databases and remove all access rights associated with them
-drop database if exists AIL_Base_DataSource_Master_Motor;
-drop database if exists AIL_Base_DataSource_Test;
-drop database if exists MetropolitanInsurance_Motor_Staging;
-drop database if exists StarInsurance_Motor_Staging;
+drop database if exists ##dbname##_AIL_Base_DataSource_Master_Motor;
+drop database if exists ##dbname##_AIL_Base_DataSource_Test;
+drop database if exists ##dbname##_MetropolitanInsurance_Motor_Staging;
+drop database if exists ##dbname##_StarInsurance_Motor_Staging;
 
 revoke all privileges, grant option from 'charlie'@'localhost';
 revoke all privileges, grant option from 'lily'@'localhost';
@@ -10,4 +10,5 @@ revoke all privileges, grant option from 'oliver'@'localhost';
 revoke all privileges, grant option from 'ethan'@'localhost';
 revoke all privileges, grant option from 'sophie'@'localhost';
 revoke all privileges, grant option from 'thomas'@'localhost';
+revoke all privileges, grant option from 'report'@'localhost';
 
